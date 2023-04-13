6cbf231f2d966e2e0d2bb200cb4eac24bdf5eefb
Fer <Fmeji@hotmail.es>
2023-04-13 12:47:51 +0200

    Atributos Battleship creados

--- src/main/java/Battleship.java
+++ src/main/java/Battleship.java
@@ -1,4 +1,10 @@
 import java.awt.*;
 
 public class Battleship extends Ship {
+    private boolean[] hitPoints;
+
+    public Battleship(Point start, Point end) {
+        super(5, start, end);
+        this.hitPoints = new boolean[5];
+    }
 }
