486a71d1f2079f74b1cfd8a0c078c5a065a4ad07
Fer <Fmeji@hotmail.es>
2023-04-13 12:23:07 +0200

    Clase User creada

--- src/main/java/Ship.java
+++ src/main/java/Ship.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.awt.Point;
 
 public abstract class Ship {
     protected int hitPoints;
@@ -6,4 +6,99 @@ public abstract class Ship {
     protected Point start;
     protected Point end;
     protected CardinalPoints direction;
+
+    public Ship(int size, Point start, Point end) {
+        this.size = size;
+        this.hitPoints = 0;
+        this.start = start;
+        this.end = end;
+        if (start.getX() == end.getX()) {
+            this.direction = CardinalPoints.NORTH;
+        } else if (start.getY() == end.getY()) {
+            this.direction = CardinalPoints.EAST;
+        } else {
+            throw new IllegalArgumentException("Los barcos no están alineados correctamente");
+        }
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Point getStart() {
+        return start;
+    }
+
+    public void setStart(Point start) {
+        this.start = start;
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public void setEnd(Point end) {
+        this.end = end;
+    }
+
+    public CardinalPoints getDirection() {
+        return direction;
+    }
+
+    public void setDirection(CardinalPoints direction) {
+        this.direction = direction;
+    }
+
+    public boolean isSunk() {
+        return hitPoints >= size;
+    }
+
+    public boolean getShot(Point shotPoint) {
+        if (direction == CardinalPoints.NORTH) {
+            if (shotPoint.getX() != start.getX()) {
+                return false;
+            }
+            if (shotPoint.getY() >= start.getY() && shotPoint.getY() <= end.getY()) {
+                hitPoints++;
+                return true;
+            }
+        } else if (direction == CardinalPoints.EAST) {
+            if (shotPoint.getY() != start.getY()) {
+                return false;
+            }
+            if (shotPoint.getX() >= start.getX() && shotPoint.getX() <= end.getX()) {
+                hitPoints++;
+                return true;
+            }
+        } else if (direction == CardinalPoints.SOUTH) {
+            if (shotPoint.getX() != start.getX()) {
+                return false;
+            }
+            if (shotPoint.getY() >= end.getY() && shotPoint.getY() <= start.getY()) {
+                hitPoints++;
+                return true;
+            }
+        } else if (direction == CardinalPoints.WEST) {
+            if (shotPoint.getY() != start.getY()) {
+                return false;
+            }
+            if (shotPoint.getX() >= end.getX() && shotPoint.getX() <= start.getX()) {
+                hitPoints++;
+                return true;
+            }
+        }
+        return false;
+    }
 }
