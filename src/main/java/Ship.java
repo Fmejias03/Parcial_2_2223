6ec0dd58d453a51d628ecb0cc911268b06ff67f7
Fer <Fmeji@hotmail.es>
2023-04-13 11:55:35 +0200

    Atributos creados

--- src/main/java/Ship.java
+++ src/main/java/Ship.java
@@ -1,5 +1,9 @@
+import java.awt.*;
+
 public abstract class Ship {
-
-
-
+    protected int hitPoints;
+    protected int size;
+    protected Point start;
+    protected Point end;
+    protected CardinalPoints direction;
 }
