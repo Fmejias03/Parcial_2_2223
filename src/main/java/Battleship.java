9b2fc65f53c6d63e85190a7b5340d40d49d14f17
Fer <Fmeji@hotmail.es>
2023-04-13 17:45:34 +0200

    Clase Battleship Terminada

--- src/main/java/Battleship.java
+++ src/main/java/Battleship.java
@@ -1,10 +1,62 @@
 import java.awt.*;
 
 public class Battleship extends Ship {
-    private boolean[] hitPoints;
+    private boolean[] isolatedContainers;
 
     public Battleship(Point start, Point end) {
         super(5, start, end);
-        this.hitPoints = new boolean[5];
+        isolatedContainers = new boolean[3];
+        if (start.getX() == end.getX()) {
+            direction = CardinalPoints.NORTH;
+            isolatedContainers[0] = true;
+            isolatedContainers[2] = true;
+        } else {
+            direction = CardinalPoints.EAST;
+            isolatedContainers[1] = true;
+            isolatedContainers[2] = true;
+        }
     }
+
+    public boolean[] getIsolatedContainers() {
+        return isolatedContainers;
+    }
+
+    public boolean getShot(Point shotPoint) {
+        if (direction == CardinalPoints.NORTH) {
+            if (shotPoint.getX() != start.getX()) {
+                return false;
+            }
+            int container = (int) (shotPoint.getY() - start.getY());
+            if (container < 0 || container > 4) {
+                return false;
+            }
+            if (!isolatedContainers[container / 2]) {
+                return false;
+            }
+            isolatedContainers[container / 2] = false;
+            hitPoints++;
+            if (hitPoints == 5) {
+                return true;
+            }
+        } else {
+            if (shotPoint.getY() != start.getY()) {
+                return false;
+            }
+            int container = (int) (shotPoint.getX() - start.getX());
+            if (container < 0 || container > 4) {
+                return false;
+            }
+            if (!isolatedContainers[container / 2]) {
+                return false;
+            }
+            isolatedContainers[container / 2] = false;
+            hitPoints++;
+            if (hitPoints == 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
