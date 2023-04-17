d200b88c533ffb7ee17cbdf6adec31dc99c565f3
Fer <Fmeji@hotmail.es>
2023-04-17 21:29:39 +0200

    Clase Hash para hashmap

--- src/main/java/Hash.java
+++ src/main/java/Hash.java
@@ -1,20 +1,2 @@
 public class Hash {
-    /*Escribir un algoritmo que permita utilizar tres tablas hash para guardar los datos de barcos, que
-contemple las siguientes actividades:
-a. En la primera tabla hash la función hash debe ser sobre el tipo de barco, en la segunda tabla la
-función hash deberá utilizar el número del barco como clave, mientras que en la tercera el campo
-clave de la función hash será por el nombre del barco.
-b. El tamaño de la primera tabla debe ser lo suficientemente grande como para que pueda
-almacenar todos los distintos tipos de barcos, debe manejar las colisiones con alguna función de
-sondeo.
-c. El tamaño de cada una de las segundas tablas debe ser 15.
-d. El algoritmo debe permitir cargar tipos de barcos en la primera tabla y crear su respectiva segunda
-tabla, en el caso de que no exista.
-e. Si el barco es de más de un tipo deberá cargarlo en cada una de las tablas que indiquen estos
-tipos.
-f. Deberá permitir cargar barcos de los cuales se dispone de su número, nombre, tipo y nivel.*/
-
-
-
-
 }
